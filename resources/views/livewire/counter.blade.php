d4c87d1a602d230f64c84b9aee047f6c29621436
AeZa123 <aymexe1212@gmail.com>
2021-12-12 20:31:18 +0700

    ทำการทดสอบ livewire โดยการสร้างฟังก์ชันนับเลข

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,8 @@
-<div>
+<div style="text-align: center">
     {{-- Because she competes with no one, no one can compete with her. --}}
+
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+    <button wire:click="decrement">-</button>
+
 </div>
